2e01927e5882ec3bd1ae463d4f2bff7bc4d3f5de
suyog100 <suyogmanandhar2022@gmail.com>
2024-04-19 11:54:58 +0545

    adding and find simple interest

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+
+import 'package:batch_32b/screens/simple_interest.dart';
+import 'package:flutter/material.dart';
+
+import '../screens/arithmetic_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SimpleInterest(),
+    );
+  }}
